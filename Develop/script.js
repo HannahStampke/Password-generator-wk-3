71acb320e82d4585ccd8fba1928ea56cdbb914b0
Hannah Stampke <hannah.stampke@gmail.com>
2022-03-29 00:11:44 +1030

    added initial generatePassword function and No, ALL, or 3 password option results

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var enter;
+var options;
 var passwordMinLength = 8;
 var passwordMaxLength = 128;
+// Password options
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "1234567890";
@@ -9,6 +12,42 @@ var specialChar = "'!@#$%^&*()_+-=~`{}[]|\:;<>,.?/'";
 
 // Function to generate password
 function generatePassword() {
+  enter = parseInt(prompt("How many characters? (8-128)"));
+  if (!enter) {
+    alert("Please enter a number");
+  }
+  // Checks that password is the correct length
+  else if (enter < passwordMinLength || enter > passwordMaxLength) {
+    enter = parseInt(prompt("Please choose between 8 and 128"));
+  }
+  // If length is correct, offer the password options
+  else {
+    lowerCase = confirm("Will this contain lower case letters?");
+    upperCase = confirm("Will this contain upper case letters?");
+    numeric = confirm("Will this contain numbers?");
+    specialChar = confirm("Will this contain special characters?");
+  };
+  // If NO password options are chosen
+  if (!lowerCase && !upperCase && !numeric && !specialChar) {
+    options = alert("Well it has to contain something!");
+  }
+  // If ALL password options are chosen
+  else if (lowerCase && upperCase && numeric && specialChar) {
+    options = lowerCase.concat(upperCase, numeric, specialChar);
+  }
+  // If 3 password options are chosen
+  else if (lowerCase && upperCase && numeric) {
+    options = lowerCase.concat(upperCase, numeric);
+  }
+  else if (lowercase && upperCase && specialChar); {
+    options = lowerCase.concat(upperCase, specialChar);
+  }
+  else if (lowercase && numeric && specialChar); {
+    options = lowerCase.concat(numeric, specialChar);
+  }
+  else if (upperCase && numeric && specialChar); {
+    options = upperCase.concat(numeric, specialChar);
+  }
   
 }
 
